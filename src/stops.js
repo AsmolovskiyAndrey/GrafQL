735070881ea017b5dca5501f43ab246a9727f93b
Andrii <105419171+AsmolovskiyAndrey@users.noreply.github.com>
2023-07-26 12:26:08 +0200

    Create stops.js

---
+++ src/stops.js
@@ -0,0 +1,13 @@
+import fs from "fs";
+
+const syncData = fs.readFileSync("./data/Haltestellen.csv", "utf8");
+const stopsWithId = {};
+const dataStr = syncData.split("\r");
+
+dataStr.forEach((element) => {
+        const hafasId = element.split("\t")[1];
+        const langName = element.split("\t")[2];
+
+        stopsWithId[hafasId.slice(1, -1)] = langName.slice(1, -1);
+});
+export const arrayEntries = Object.entries(stopsWithId)
