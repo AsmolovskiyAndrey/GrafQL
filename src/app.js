72be67137b7ff83db87bee35783068c94c0e36eb
Andrii <105419171+AsmolovskiyAndrey@users.noreply.github.com>
2023-07-26 12:25:19 +0200

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,40 +1,15 @@
-import dotenv from 'dotenv';
-import path from 'path';
-import { fileURLToPath } from 'url';
-
-// import {InMemoryCache} from "apollo-cache-inmemory";
-// import {HttpLink} from "apollo-link-http";
-// import {ApolloLink} from "apollo-link";
-import {setContext} from "apollo-link-context";
-// import { setContext } from "@apollo/client/link/context";
 import gql from "graphql-tag";
-import fetch from "node-fetch";
 import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
-// import pkg from "apollo-client";
-import pkg from '@apollo/client';
-// import { ApolloClient, InMemoryCache, ApolloLink, HttpLink } from '@apollo/client'
-import { log } from 'console';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const envPath = path.resolve(__dirname, '../.env');
-dotenv.config({ path: envPath });
-// import pkg from '@apollo/client';
-const { ApolloClient, InMemoryCache, ApolloLink, HttpLink } = pkg;
-
-const CLIENT_ID = process.env.clientID;
-const tenantID = process.env.tenantID;
-const CLIENT_SECRET = process.env.clientSecret;
-const RESOURCE_ID = process.env.resource;
-const HOSTNAME = process.env.hostname;
-const OAUTH_URL = `https://login.microsoftonline.com/{${tenantID}}/oauth2/token`;
+import { accessToken, client} from "./middleware/authorization.js";
+import { arrayEntries } from "./stops.js";
 
 const typeDefs = `#graphql
     type Query {
         token: String
         station(id: ID): stationWithId
         stations: stationsNearby
+        searchStation(search: String): [stationWithId]
         }
 
     type stationWithId {
@@ -56,50 +31,7 @@ const typeDefs = `#graphql
     }
 `;
 
-const accessToken = async () => {
-    const promise = new Promise(async (resolve, reject) => {
-        const response = await fetch(OAUTH_URL, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/x-www-form-urlencoded",
-        },
-        body: `grant_type=client_credentials
-            &client_id=${CLIENT_ID}
-            &client_secret=${CLIENT_SECRET}
-            &resource=${RESOURCE_ID}`,
-        });
-
-        resolve(response.json());
-    });
-
-    const json = await promise;
-    return json["access_token"];
-};
-
-const httpLink = new HttpLink({
-    uri: HOSTNAME,
-    credentials: "same-origin",
-    fetch: fetch,
-});
-
-const authMiddleware = setContext(
-    (request) =>
-        new Promise(async (resolve, reject) => {
-        const token = await accessToken();
-        resolve({
-            headers: {
-            authorization: `Bearer ${token}`,
-            },
-        });
-    })
-);
-
-const client = new ApolloClient({
-    link: ApolloLink.from([authMiddleware, httpLink]),
-    cache: new InMemoryCache(),
-});
-
-const getStationNotWarAway = () => client.query({
+const getStationNotFarAway = () => client.query({
         query: gql`
         query {
             stations(first: 3, lat: 49.483076, long: 8.468409, distance: 0.5) {
@@ -116,7 +48,7 @@ const getStationNotWarAway = () => client.query({
         `,
 }).then((result) => (result["data"]));
 
-const getStationWithId = (hafasID) => client.query({
+const getStationWithId = (hafasID = 2171) => client.query({
     query: gql`
         query {
             station(id: "${hafasID}") {
@@ -136,14 +68,34 @@ const resolvers = {
         },
         station: async (parent, args) => {
             const stationId = await Object(getStationWithId(args.id));
-            // console.log(args.id);
-            // console.log(stationId.station);
             return stationId.station
         },
         stations: async () => {
-            const stationNotWar = await Object(getStationNotWarAway());
-            // console.log(stationNotWar);
+            const stationNotWar = await Object(getStationNotFarAway());
             return stationNotWar.stations
+        },
+        searchStation: async (parent, args) => {
+            const searchValue = args.search.toLowerCase().trim();
+            const arrayWithHafasID = [];
+            const resultArrayWithStopsData = [];
+
+            const result = arrayEntries.filter((element) => 
+                element[1].toLowerCase().includes(searchValue)
+            );
+
+            result.forEach((item)=>{
+                arrayWithHafasID.push(item[0]);
+            })
+            
+            async function stationId() {
+                for (const item of arrayWithHafasID) {
+                    const stationWithSpecificId = await Object(getStationWithId(item));
+                    resultArrayWithStopsData.push(stationWithSpecificId.station)
+                }
+            };
+            await stationId();
+
+            return resultArrayWithStopsData
         }
     },
     };
