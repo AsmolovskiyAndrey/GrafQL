4c22fd1e8c609d1be895b1704bc6ca970244d0b1
Andrii <105419171+AsmolovskiyAndrey@users.noreply.github.com>
2023-08-03 08:07:21 +0200

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,109 +1,26 @@
-import gql from "graphql-tag";
-import { ApolloServer } from '@apollo/server';
-import { startStandaloneServer } from '@apollo/server/standalone';
-import { accessToken, client} from "./middleware/authorization.js";
+import express from "express";
 import { arrayEntries } from "./stops.js";
+import { getStationWithId } from "./appWithGraphql.js";
 
-const typeDefs = `#graphql
-    type Query {
-        token: String
-        station(id: ID): stationWithId
-        stations: stationsNearby
-        searchStation(search: String): [stationWithId]
-        }
+const app = express();
 
-    type stationWithId {
-        hafasID: ID,
-        longName: String,
-        shortName: String,
-        name: String,
+app.get("/api/stations/search/",async (req, res) => {
+    let searchValue = req.query.key.toLowerCase();
+    const resultsIdUndName = arrayEntries.filter((el) =>
+        el[1].toLowerCase().includes(searchValue)
+    );
+
+    const resultWithDataAboutStops = [];
+
+    for (const item of resultsIdUndName) {
+        const dataAboutOneStop = await getStationWithId(item[0]);
+        resultWithDataAboutStops.push(dataAboutOneStop)
     }
+    res.json(resultWithDataAboutStops);
+});
 
-    type stationsNearby {
-        totalCount: Int,
-        elements:[stationsInfo]
-    }
+app.listen(3000, (err) => {
+    if (err) console.error("Error at server launch:", err);
 
-    type stationsInfo {
-        hafasID: String,
-        globalID: String,
-        longName: String,
-    }
-`;
-
-const getStationNotFarAway = () => client.query({
-        query: gql`
-        query {
-            stations(first: 3, lat: 49.483076, long: 8.468409, distance: 0.5) {
-            totalCount
-            elements {
-                ... on Station {
-                hafasID
-                globalID
-                longName
-                }
-            }
-            }
-        }
-        `,
-}).then((result) => (result["data"]));
-
-const getStationWithId = (hafasID = 2171) => client.query({
-    query: gql`
-        query {
-            station(id: "${hafasID}") {
-                hafasID
-                longName
-                shortName
-                name
-        } }
-    `,
-}).then(result => result["data"]);
-
-const resolvers = {
-    Query: {
-        token: async () => {
-            const tokenAccess = await accessToken();
-            return tokenAccess;
-        },
-        station: async (parent, args) => {
-            const stationId = await Object(getStationWithId(args.id));
-            return stationId.station
-        },
-        stations: async () => {
-            const stationNotWar = await Object(getStationNotFarAway());
-            return stationNotWar.stations
-        },
-        searchStation: async (parent, args) => {
-            const searchValue = args.search.toLowerCase().trim();
-            const arrayWithHafasID = [];
-            const resultArrayWithStopsData = [];
-
-            const result = arrayEntries.filter((element) => 
-                element[1].toLowerCase().includes(searchValue)
-            );
-
-            result.forEach((item)=>{
-                arrayWithHafasID.push(item[0]);
-            })
-            
-            async function stationId() {
-                for (const item of arrayWithHafasID) {
-                    const stationWithSpecificId = await Object(getStationWithId(item));
-                    resultArrayWithStopsData.push(stationWithSpecificId.station)
-                }
-            };
-            await stationId();
-
-            return resultArrayWithStopsData
-        }
-    },
-    };
-
-const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-    });
-    
-const { url } = await startStandaloneServer(server);
-console.log(`🚀 Server ready at ${url}`);
+    console.log("🚀 Server ready at http://localhost:3000/ for GET requests");
+});
