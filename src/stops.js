f40576d45d3cde7a2ccd034fecd2873970a4d17b
Andrii <105419171+AsmolovskiyAndrey@users.noreply.github.com>
2023-08-03 08:08:17 +0200

    Update stops.js

--- src/stops.js
+++ src/stops.js
@@ -1,15 +1,18 @@
 import fs from "fs";
+import path from "path";
 
-const syncData = fs.readFileSync("../data/Haltestellen.csv", "utf8");
+const rightPath = path.resolve('/Users/andriiasmolovskyi/Desktop/workspace/zweite Aufgabe/graphql_aufgabe/data/Haltestellen.csv')
+
+const syncData = fs.readFileSync(rightPath, "utf8");
 const stopsWithId = {};
 const dataStr = syncData.split("\r");
 
 dataStr.forEach((element) => {
         const hafasId = element.split("\t")[1];
         const langName = element.split("\t")[2];
+
         if (typeof (hafasId) !== "undefined") {
                 stopsWithId[hafasId.slice(1, -1)] = langName.slice(1, -1);
         }
-        
 });
 export const arrayEntries = Object.entries(stopsWithId)
